23b2956f9f9da7b82cff5a4f78690fea5543842a
hyrodium <hyrodium@gmail.com>
2020-03-16 15:40:56 +0900

    Files generated by PkgTemplates

---
+++ README.md
@@ -0,0 +1,5 @@
+# HelloWorld
+
+[![Stable](https://img.shields.io/badge/docs-stable-blue.svg)](https://hyrodium.github.io/HelloWorld.jl/stable)
+[![Dev](https://img.shields.io/badge/docs-dev-blue.svg)](https://hyrodium.github.io/HelloWorld.jl/dev)
+[![Build Status](https://travis-ci.com/hyrodium/HelloWorld.jl.svg?branch=master)](https://travis-ci.com/hyrodium/HelloWorld.jl)
---
+++ docs/make.jl
@@ -0,0 +1,17 @@
+using Documenter, HelloWorld
+
+makedocs(;
+    modules=[HelloWorld],
+    format=Documenter.HTML(),
+    pages=[
+        "Home" => "index.md",
+    ],
+    repo="https://github.com/hyrodium/HelloWorld.jl/blob/{commit}{path}#L{line}",
+    sitename="HelloWorld.jl",
+    authors="hyrodium <hyrodium@gmail.com>",
+    assets=String[],
+)
+
+deploydocs(;
+    repo="github.com/hyrodium/HelloWorld.jl",
+)
