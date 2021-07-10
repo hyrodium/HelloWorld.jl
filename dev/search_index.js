a46e1cab13fe8700db929efd2595613fd5d0e8d8
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-07-10 14:49:00 +0000

    build based on 09ca447

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#HelloWorld.jl-1","page":"Home","title":"HelloWorld.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [HelloWorld]","category":"page"}]
+[{"location":"#HelloWorld.jl-1","page":"Home","title":"HelloWorld.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"Ya","category":"page"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [HelloWorld]","category":"page"},{"location":"#","page":"Home","title":"Home","text":"HI","category":"page"}]
 }
