cbd7175391da0ea0a5e97cc669308a8219c1d81e
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2021-07-10 15:19:49 +0000

    build based on 61d141d

---
+++ ja/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HelloWorld.jl-1","page":"Home","title":"HelloWorld.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"にほんごのどきゅめんと","category":"page"}]
+}
--- versions.js
+++ versions.js
@@ -1,5 +1,5 @@
 var DOC_VERSIONS = [
   "stable",
   "v0.1",
-  "en",
+  "ja",
 ];
