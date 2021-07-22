bf332d2fc5ce3ec4b3999b85359e0eeb6ab5d128
Documenter.jl <documenter@juliadocs.github.io>
2021-07-22 13:17:14 +0000

    build based on 79a97d6

---
+++ v0.2.1/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#HelloWorld.jl","page":"Home","title":"HelloWorld.jl","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"English docs","category":"page"}]
+}
